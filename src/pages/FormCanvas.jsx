146701cdeb0b01e62cce95ef819e6c69c156149c
himanshu-creativebuffer <himanshukarnwal@creativebuffer.com>
2025-07-07 16:22:13 +0530

    style: add fr unit ranther than auto

--- src/pages/FormCanvas.jsx
+++ src/pages/FormCanvas.jsx
@@ -4,7 +4,7 @@ import Section from './Section';
 
 const FormCanvas = ({ sections, onAddField, onSelectField, onDeleteField, onSortEnd, handleDeleteSection, handleEditSectionModalOpen }) => {
     return (
-        <div className="flex-grow p-8 bg-gray-100 rounded-lg shadow-inner border-2 border-dashed border-gray-300 min-h-[600px] overflow-auto">
+        <div className="flex flex-col gap-4 flex-grow p-8 bg-gray-100 rounded-lg shadow-inner border-2 border-dashed border-gray-300 min-h-[600px] overflow-auto">
             {sections.length === 0 ? (
                 <p className="text-gray-500 text-lg text-center mt-20">Add sections to start building your form</p>
             ) : (
